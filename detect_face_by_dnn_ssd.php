fe23e87db4335ed657d049255b727b22b223f0fc
Vladimir Goncharov <morozovsk@gmail.com>
2018-06-16 23:08:40 +0300

    phpdoc

--- detect_face_by_dnn_ssd.php
+++ detect_face_by_dnn_ssd.php
@@ -1,17 +1,12 @@
 <?php
 
 use CV\Scalar, CV\Size;
-use function CV\{imread, imwrite, cvtColor, equalizeHist, rectangle};
-use const CV\{COLOR_BGR2GRAY};
+use function CV\{imread, imwrite, rectangle};
 
 
 $src = imread("images/faces.jpg");
-$gray = cvtColor($src, COLOR_BGR2GRAY);
-equalizeHist($gray, $gray);
 
-\CV\resize($src, $resised, new Size(300, 300));
-
-$blob = \CV\DNN\blobFromImage($resised, 1, new Size(300, 300), new Scalar(104, 177, 123));
+$blob = \CV\DNN\blobFromImage($src, 1, new Size(300, 300), new Scalar(104, 177, 123), true, false);
 
 $net = \CV\DNN\readNetFromCaffe('models/ssd/res10_300x300_ssd_deploy.prototxt', 'models/ssd/res10_300x300_ssd_iter_140000.caffemodel');
 
@@ -19,11 +14,13 @@ $net->setInput($blob, "");
 
 $r = $net->forward();
 
+var_export($r->shape);
+
 $scalar = new Scalar(0, 0, 255);
 for ($i = 0; $i < $r->shape[2]; $i++) {
     $confidence = $r->atIdx([0,0,$i,2]);
     //var_export($confidence);echo "\n";
-    if ($confidence > 0.5) {
+    if ($confidence > 0.2) {
         $startX = $r->atIdx([0,0,$i,3]) * $src->cols;
         $startY = $r->atIdx([0,0,$i,4]) * $src->rows;
         $endX = $r->atIdx([0,0,$i,5]) * $src->cols;
