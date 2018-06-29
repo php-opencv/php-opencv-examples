b2d720d216a9f53dc5f6976cc8d03a78dce621e9
Vladimir Goncharov <morozovsk@gmail.com>
2018-06-29 18:59:35 +0300

    fixed detect face by dnn ssd

--- detect_face_by_dnn_ssd.php
+++ detect_face_by_dnn_ssd.php
@@ -5,8 +5,13 @@ use function CV\{imread, imwrite, rectangle};
 
 
 $src = imread("images/faces.jpg");
+$size = $src->size(); // 2000x500
 
-$blob = \CV\DNN\blobFromImage($src, 1, new Size(300, 300), new Scalar(104, 177, 123), true, false);
+$minSide = min($size->width, $size->height);
+$divider = $minSide / 300;
+\CV\resize($src, $resized, new Size($size->width / $divider, $size->height / $divider)); // 1200x300
+
+$blob = \CV\DNN\blobFromImage($resized, 1, new Size(), new Scalar(104, 177, 123), true, false);
 
 $net = \CV\DNN\readNetFromCaffe('models/ssd/res10_300x300_ssd_deploy.prototxt', 'models/ssd/res10_300x300_ssd_iter_140000.caffemodel');
 
@@ -19,8 +24,8 @@ var_export($r->shape);
 $scalar = new Scalar(0, 0, 255);
 for ($i = 0; $i < $r->shape[2]; $i++) {
     $confidence = $r->atIdx([0,0,$i,2]);
-    //var_export($confidence);echo "\n";
-    if ($confidence > 0.2) {
+    if ($confidence > 0.9) {
+        var_export($confidence);echo "\n";
         $startX = $r->atIdx([0,0,$i,3]) * $src->cols;
         $startY = $r->atIdx([0,0,$i,4]) * $src->rows;
         $endX = $r->atIdx([0,0,$i,5]) * $src->cols;
